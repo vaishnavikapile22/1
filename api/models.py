a830e02f234859fb29a17abf93b9154c71b4a3b0
vaishnavikapile22 <kapilevaishnavi22@gmail.com.com>
2024-04-21 15:04:04 +0000

    Initial commit

--- api/models.py
+++ api/models.py
@@ -1,18 +1,123 @@
 from django.db import models
+import pycountry
 
-STATE_CHOICE = ((
- ('Bihar','Bihar'),
- ('Jharkhand','Jharkhand'),
- ('West Bengal','West Bengal'),
-))
+
+# Get a list of country names
+COUNTRY_CHOICES = [(country.name, country.name) for country in pycountry.countries]
+COUNTRY_CHOICES.sort()
+# Get a list of state names for all countries
+STATE_CHOICES = (
+    # States of India
+    ('Andhra Pradesh', 'Andhra Pradesh'),
+    ('Arunachal Pradesh', 'Arunachal Pradesh'),
+    ('Assam', 'Assam'),
+    ('Bihar', 'Bihar'),
+    ('Chhattisgarh', 'Chhattisgarh'),
+    ('Goa', 'Goa'),
+    ('Gujarat', 'Gujarat'),
+    ('Haryana', 'Haryana'),
+    ('Himachal Pradesh', 'Himachal Pradesh'),
+    ('Jharkhand', 'Jharkhand'),
+    ('Karnataka', 'Karnataka'),
+    ('Kerala', 'Kerala'),
+    ('Madhya Pradesh', 'Madhya Pradesh'),
+    ('Maharashtra', 'Maharashtra'),
+    ('Manipur', 'Manipur'),
+    ('Meghalaya', 'Meghalaya'),
+    ('Mizoram', 'Mizoram'),
+    ('Nagaland', 'Nagaland'),
+    ('Odisha', 'Odisha'),
+    ('Punjab', 'Punjab'),
+    ('Rajasthan', 'Rajasthan'),
+    ('Sikkim', 'Sikkim'),
+    ('Tamil Nadu', 'Tamil Nadu'),
+    ('Telangana', 'Telangana'),
+    ('Tripura', 'Tripura'),
+    ('Uttar Pradesh', 'Uttar Pradesh'),
+    ('Uttarakhand', 'Uttarakhand'),
+    ('West Bengal', 'West Bengal'),
+    ('Andaman and Nicobar Islands', 'Andaman and Nicobar Islands'),
+    ('Chandigarh', 'Chandigarh'),
+    ('Dadra and Nagar Haveli', 'Dadra and Nagar Haveli'),
+    ('Daman and Diu', 'Daman and Diu'),
+    ('Lakshadweep', 'Lakshadweep'),
+    ('Delhi', 'Delhi'),
+    ('Puducherry', 'Puducherry'),
+
+    # States of the United States
+    ('Alabama', 'Alabama'),
+    ('Alaska', 'Alaska'),
+    ('Arizona', 'Arizona'),
+    ('Arkansas', 'Arkansas'),
+    ('California', 'California'),
+    ('Colorado', 'Colorado'),
+    ('Connecticut', 'Connecticut'),
+    ('Delaware', 'Delaware'),
+    ('Florida', 'Florida'),
+    ('Georgia', 'Georgia'),
+    ('Hawaii', 'Hawaii'),
+    ('Idaho', 'Idaho'),
+    ('Illinois', 'Illinois'),
+    ('Indiana', 'Indiana'),
+    ('Iowa', 'Iowa'),
+    ('Kansas', 'Kansas'),
+    ('Kentucky', 'Kentucky'),
+    ('Louisiana', 'Louisiana'),
+    ('Maine', 'Maine'),
+    ('Maryland', 'Maryland'),
+    ('Massachusetts', 'Massachusetts'),
+    ('Michigan', 'Michigan'),
+    ('Minnesota', 'Minnesota'),
+    ('Mississippi', 'Mississippi'),
+    ('Missouri', 'Missouri'),
+    ('Montana', 'Montana'),
+    ('Nebraska', 'Nebraska'),
+    ('Nevada', 'Nevada'),
+    ('New Hampshire', 'New Hampshire'),
+    ('New Jersey', 'New Jersey'),
+    ('New Mexico', 'New Mexico'),
+    ('New York', 'New York'),
+    ('North Carolina', 'North Carolina'),
+    ('North Dakota', 'North Dakota'),
+    ('Ohio', 'Ohio'),
+    ('Oklahoma', 'Oklahoma'),
+    ('Oregon', 'Oregon'),
+    ('Pennsylvania', 'Pennsylvania'),
+    ('Rhode Island', 'Rhode Island'),
+    ('South Carolina', 'South Carolina'),
+    ('South Dakota', 'South Dakota'),
+    ('Tennessee', 'Tennessee'),
+    ('Texas', 'Texas'),
+    ('Utah', 'Utah'),
+    ('Vermont', 'Vermont'),
+    ('Virginia', 'Virginia'),
+    ('Washington', 'Washington'),
+    ('West Virginia', 'West Virginia'),
+    ('Wisconsin', 'Wisconsin'),
+    ('Wyoming', 'Wyoming'),
+
+    # States of Australia
+    ('New South Wales', 'New South Wales'),
+    ('Queensland', 'Queensland'),
+    ('South Australia', 'South Australia'),
+    ('Tasmania', 'Tasmania'),
+    ('Victoria', 'Victoria'),
+    ('Western Australia', 'Western Australia'),
+)
+
+GENDER_CHOICES = (
+    ('Female', 'Female'),
+    ('Male', 'Male'),
+    ('Transgender', 'Transgender')
+)
+
 
 class Profile(models.Model):
-  name= models.CharField(max_length=100)
-  email = models.EmailField()
-  dob = models.DateField(auto_now=False, auto_now_add=False)
-  state = models.CharField(choices=STATE_CHOICE, max_length=50)
-  gender= models.CharField(max_length=100)
-  location= models.CharField(max_length=100)
-  pimage = models.ImageField(upload_to='pimages', blank=True)
-  rdoc = models.FileField(upload_to='rdocs', blank=True)
-
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+    dob = models.DateTimeField()  # Changed to DateField for date of birth
+    country = models.CharField(choices=COUNTRY_CHOICES, max_length=50)
+    state = models.CharField(choices=STATE_CHOICES, max_length=50)
+    gender = models.CharField(choices=GENDER_CHOICES, max_length=50)
+    image = models.ImageField(upload_to = 'image/')
+    file = models.FileField(upload_to = 'file/')
