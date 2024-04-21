740a71d6facc7f0afc472a7635f72246141685b8
vaishnavikapile22 <149785862+vaishnavikapile22@users.noreply.github.com>
2024-04-21 20:40:39 +0530

    Update serializers.py

--- api/serializers.py
+++ api/serializers.py
@@ -1,8 +1,9 @@
-from pyexpat import model
+
 from rest_framework import serializers
 from api.models import Profile
 
+
 class ProfileSerializer(serializers.ModelSerializer):
-  class Meta:
-    model=Profile
-    fields = ['id', 'name', 'email', 'dob', 'state', 'gender', 'location', 'pimage', 'rdoc']
+    class Meta:
+        model = Profile
+        fields = ['id','name', 'email','dob','country','state','gender','location','image','file']
